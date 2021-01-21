5709753c5715bced0a166cade2c87eefc7706aac
Max DeVos <devosmaxwell@gmail.com>
2021-01-20 19:41:10 -0600

    Added OBS Client,

---
+++ public/scripts/obs.js
@@ -0,0 +1,66 @@
+/**
+ * Status: Needs CSGO Integration
+ * Audio In: Caster
+ * Video In: Player
+ * Audio Out: No
+ * Video Out: No
+ */
+
+let lastObservedPlayer = "";
+
+// ======================== RTC Bullshit Starts Here ============================
+
+function configUser(socket){
+    socket.on('connect', () => {
+        console.log("Connected!");
+        socket.emit("obs-con", function(data) {
+            console.log(data);
+        });
+    });
+
+    socket.on('obs-invalid', () => {
+        document.open();
+        document.write('<h1 style="text-align: center;color:red">Caster Already Connected</h1>');
+        document.close();
+        socket.disconnect();
+    });
+
+    socket.on('initReceive', remoteData => {
+        console.log('INIT RECEIVE FROM ' + remoteData.socket_id + ":" + remoteData.type);
+        if(remoteData.type === "player"){
+            addPeer(remoteData.socket_id, false, true);
+        }
+        else{
+            addPeer(remoteData.socket_id, false, false);
+        }
+        socket.emit('initSend', {socket_id: remoteData.socket_id, type: "obs"})
+    })
+
+    socket.on('new-observed-player', playerSocket => {
+        try{
+            for(let i in peers){
+                document.getElementById(i).style.position = "absolute";
+                if(i === playerSocket){
+                    document.getElementById(i).style.visibility = "visible";
+                }
+                else{
+                    document.getElementById(i).style.visibility = "hidden";
+                }
+            }
+        }catch (e){
+            console.log("Error Handling Player Camera Switch: ", playerSocket);
+            console.log(e);
+        }
+    })
+
+
+
+    // createUserListener('observer', socket);
+    // createUserListener('broadcaster', socket);
+    // createUserListener('caster1', socket);
+    // createUserListener('caster2', socket);
+    // createUserListener('player1', socket);
+    // createUserListener('player2', socket);
+    // createUserListener('player3', socket);
+    // createUserListener('player4', socket);
+}
