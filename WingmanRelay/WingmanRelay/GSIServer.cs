6a2f7fcfd26d508dc34da095605335e5324fdbf3
Max DeVos <devosmaxwell@gmail.com>
2021-02-08 01:26:09 -0600

    Update code style

--- WingmanRelay/WingmanRelay/GSIServer.cs
+++ WingmanRelay/WingmanRelay/GSIServer.cs
@@ -1,33 +1,31 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace WingmanRelay
-{
-class GSIServer {
-    
+namespace WingmanRelay {
+    internal class GsiServer {
         private HttpListener listener;
         private HttpClient client;
-        private string url = "http://localhost:3000/";
-        private string serverUrl = "http://13.58.40.89:3254/";
         
+        private const string url = "http://localhost:3000/";
+        private const string serverUrl = "http://13.58.40.89:3254/";
         // public static string serverUrl = "http://localhost:3254/";
+        
         private string latestData = "";
-        private bool debugMode = false;
 
-        private string getRequestData(HttpListenerRequest request)
-        {
-            if (!request.HasEntityBody)
-            {
+        private static string getRequestData(HttpListenerRequest request) {
+            if (!request.HasEntityBody) {
                 Console.WriteLine("No client data was sent with the request.");
                 return "";
             }
-            var body = request.InputStream;
-            var encoding = request.ContentEncoding;
-            var reader = new System.IO.StreamReader(body, encoding);
-            if (request.ContentType != null)
-            {
+
+            Stream body = request.InputStream;
+            Encoding encoding = request.ContentEncoding;
+            var reader = new StreamReader(body, encoding);
+            if (request.ContentType != null) {
             }
 
             // Convert the data to a string and display it on the console.
@@ -39,33 +37,26 @@ class GSIServer {
             return s;
         }
 
-        private async Task handleIncomingConnections()
-        {
+        private async Task handleIncomingConnections() {
+            
             // While a user hasn't visited the `shutdown` url, keep on handling requests
-            while (true)
-            {
+            while (true) {
                 // Will wait here until we hear from a connection
-                var ctx = listener.GetContext();
+                HttpListenerContext ctx = listener.GetContext();
 
                 // Peel out the requests and response objects
-                var req = ctx.Request;
-                var resp = ctx.Response;
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
 
                 // If it starts with this, it is a request from the server
                 var response = getRequestData(req);
                 latestData = response;
                 await client.PostAsync(serverUrl, new StringContent(latestData));
-                if (debugMode)
-                {
-                    Console.Write(latestData);
-
-                }
                 resp.Close();
             }
         }
 
-        public void start()
-        {
+        public void start() {
             // Create a Http server and start listening for incoming connections
             listener = new HttpListener();
             listener.Prefixes.Add(url);
@@ -74,7 +65,7 @@ class GSIServer {
             client = new HttpClient();
 
             // Handle requests
-            var listenTask = handleIncomingConnections();
+            Task listenTask = handleIncomingConnections();
             listenTask.GetAwaiter().GetResult();
 
             // Close the listener
--- WingmanRelay/WingmanRelay/MainClass.cs
+++ WingmanRelay/WingmanRelay/MainClass.cs
@@ -4,7 +4,7 @@
         
         public static void Main(string[] args)
         {
-            var server = new GSIServer();
+            var server = new GsiServer();
             server.start();
         }
 
